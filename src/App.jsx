108a164166e80340fba7796b1e050eeff7d8bf1f
luis miguel <luismiguelxd09@gmail.com>
2023-11-13 19:47:47 -0400

    p

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css'
+import img from './img/img2.png'
+
+function App() {
+  
+
+  return (
+    <>
+      <img src={img} alt="" srcset="" width={'100%'} />
+      <h1>En mantenimiento</h1>
+    </>
+  )
+}
+
+export default App
